3900fa253a25a7f57f8cd61acbf49c33d221acfa
Sebastian Clarke <SebClarke1@outlook.com>
2022-12-11 12:22:20 +0000

    Added generic functionality to node + removed NodeData class

--- Models/Node.cs
+++ Models/Node.cs
@@ -7,34 +7,34 @@ using System.Threading.Tasks;
 namespace Generic_Tree_Mapping__GTM_.Models
 {
     // A class representing a node in a tree.
-    internal class Node
+    internal class Node<NodeData>
     {
         // The value of the node.
         private NodeData data;
         // A list of the node's children.
-        private List<Node> children;
+        private List<Node<NodeData>> children;
 
         // Constructs a new node with the given value.
-        public Node(NodeData value)
+        public Node(NodeData data)
         {
-            this.data = value;
-            this.children = new List<Node>();
+            this.data = data;   
+            this.children = new List<Node<NodeData>>();
         }
 
         // Adds a child node to the current node.
-        public void addChild(Node child)
+        public void addChild(Node<NodeData> child)
         {
             this.children.Add(child);
         }
 
         // Removes a child node from the current node.
-        public void removeChild(Node child)
+        public void removeChild(Node<NodeData> child)
         {
             this.children.Remove(child);
         }
 
         // Gets all children from the current node.
-        public List<Node> getChildren()
+        public List<Node<NodeData>> getChildren()
         {
             return this.children;
         }
